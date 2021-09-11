1a72ab044074c3aa0ebfce7bf3a0bc761cd4b3ad
Kireita <kees.rodriguez@gmail.com>
2021-09-11 04:58:29 +0200

    a working debugger

--- js/renderer.js
+++ js/renderer.js
@@ -1,120 +1,28 @@
-//var ps = require("python-shell")
-let { PythonShell } = require('python-shell')
-let moment = require('moment-timezone')
-let play = require('audio-play');
-let load = require('audio-loader');
+const { app, BrowserWindow } = require('electron')
+const amazontts = require('extra-amazontts');
 
-let twitchViewerList = new PythonShell('python/Twitch/twitchViewerList.py');
-let twitch = new PythonShell('python/Twitch/twitch.py');
+function createWindow() {
+    window = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    })
 
-// List of Twitch viewer
-twitchViewerList.on('message', function(message) {
+    window.loadFile('index.html')
+}
 
-    console.log(message);
+try {
+    require('electron-reloader')(module)
+} catch (_) {}
 
-    var json_obj = JSON.parse(message);
 
-    getTwitchModerators(json_obj);
-    getTwitchViewers(json_obj);
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
-
-function getTwitchViewers(json_obj) {
-    let viewerlist = document.getElementById("viewers");
-
-    // Remove current list of viewers
-    if (viewerlist) {
-        while (viewerlist.firstChild) {
-            viewerlist.removeChild(viewerlist.firstChild);
-        }
-    }
-
-    // Print list of current viewers
-    let viewers = json_obj.viewers.viewers;
-    if (viewers.length > 0) {
-        for (var i = 0; i < viewers.length; i++) {
-            var li = document.createElement("li");
-            li.innerText = viewers[i];
-            viewerlist.appendChild(li);
-        }
-    }
-}
-
-function getTwitchModerators(json_obj) {
-    let viewerlist = document.getElementById("moderators");
-
-    // Remove current list of moderators
-    if (viewerlist) {
-        while (viewerlist.firstChild) {
-            viewerlist.removeChild(viewerlist.firstChild);
-        }
-    }
-
-    // Print list of current moderators
-    let viewers = json_obj.viewers.moderators;
-    if (viewers.length > 0) {
-        for (var i = 0; i < viewers.length; i++) {
-            var li = document.createElement("li");
-            li.innerText = viewers[i];
-            viewerlist.appendChild(li);
-        }
-    }
-}
-
-// Recieve Twitch chat messages
-twitch.on('message', function(message) {
-
-    let userHtml = ``
-
-    console.log(message);
-
-    load('sounds/alert.mp3').then(play);
-
-    if (JSON.parse(message).Type === "Message") {
-        // Create chat message from recieved data
-        userHtml = `
-        <article class="msg-container msg-remote" id="msg-0">
-            <div class="msg-box">
-            <div class="icon-container">
-            <img class="user-img" id="user-0" src="` + JSON.parse(message).Logo + `" />
-            <img class="status-circle" id="user-0" src="./images/twitch-icon.png" />
-        </div>
-                <div class="flr">
-                    <div class="messages">
-                    <span class="timestamp"><span class="username">` + JSON.parse(message).User + `</span><span class="posttime">` + moment().format('hh:mm') + `</span></span>
-                    <br>
-                        <p class="msg" id="msg-0">
-                        ` + JSON.parse(message).Message + ` <img class="scale" src="https://static-cdn.jtvnw.net/emoticons/v2/191313/default/dark/1.0" />
-                        </p>
-                    </div>
-
-                </div>
-            </div>
-        </article>`;
-
-    }
-
-    if (JSON.parse(message).Type === "Console") {
-        // Create chat message from recieved data
-        userHtml = `
-        <article class="msg-container msg-remote" id="msg-0">
-            <div class="msg-box">
-                <div class="flr">
-                    <div class="messages">
-                            <p class="msg" id="msg-0">
-                            ` + JSON.parse(message).Message + ` 
-                            </p>
-                    </div>
-                </div>
-            </div>
-        </article>`;
-    }
-
-
-    // Appends the message to the main chat box (shows the message)
-    $("#chatbox").append(userHtml);
-
-    // Auto-scrolls the window to the last recieved message
-    let [lastMsg] = $('.msg-container').last();
-    lastMsg.scrollIntoView({ behavior: 'smooth' });
-
-});
