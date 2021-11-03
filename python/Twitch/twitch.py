09bd38bc67896751fade27896fb6e5aa6fbeccd4
Felipe J. Villanueva <fvillanueva@live.com>
2021-11-03 18:02:07 -0400

    another simple test

--- python/Twitch/twitch.py
+++ python/Twitch/twitch.py
@@ -10,7 +10,7 @@ from py_linq import Enumerable
 from dotenv import dotenv_values
 from configparser import ConfigParser
 from pathlib import Path
-#
+###
 # Global variables
 Emote_dictionary={}
 EmoteRangeList=['']
